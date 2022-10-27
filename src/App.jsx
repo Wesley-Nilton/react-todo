b4e4e2bf0e7970e8f9e541e8c230249d20b7a778
Wesley Nilton <wesleynlimac@gmail.com>
2022-10-27 07:23:12 -0300

    Create update constants

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import './App.css';
 const API = 'https://my-json-server.typicode.com/Wesley-Nilton/react-todo/db';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [time, setTime] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
       <h1>Todo</h1>
